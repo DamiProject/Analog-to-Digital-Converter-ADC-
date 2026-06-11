a56603925bb91b140f4b8804319360dd3dca9f98
DamiProject <Damilolaawotunde95@gmail.com>
2026-06-11 11:48:01 -0400

    Correct Metadata

--- Parameters.m
+++ Parameters.m
@@ -1,14 +1,15 @@
-classdef Parameters < handle
+classdef Parameters < handle & matlab.mixin.CustomDisplay
     %% ===================================================
     %% CONTAINER OF META OBJECTS FOR ADC PARAMETERS
     %% ===================================================
-
-    properties (Access = private)
-        % Hidden storage to protect metadata integrity
+    properties (SetAccess = private)
         Params struct
     end
 
     methods
+        %% ==================================================
+        %% Constructor of the Meta Objects For ADC Parameters
+        %% ==================================================
         function obj = Parameters()
             Schema = obj.GetSchema();
             obj.Params = struct();
@@ -16,7 +17,8 @@ classdef Parameters < handle
             for k = 1:numel(Schema)
                 S = Schema{k}; 
 
-                if isfield(S, 'Unit') && ~isempty(S.Unit) && ~ismissing(S.Unit) && S.Unit ~= ""
+                if isfield(S, 'Unit') && ~isempty(S.Unit) && ...
+                    ~ismissing(S.Unit) && S.Unit ~= ""
                     obj.Params.(S.Key) = Meta( ...
                         S.Name, ...
                         S.Validator, ...
@@ -30,15 +32,55 @@ classdef Parameters < handle
                 end
             end
         end
+        %% Retrieves The Stored Value Of A Parameter Using Its Key
+        function value = getValue(obj,key)
+
+            key = char(key);
+
+            if ~isfield(obj.Params,key)
+                error('Parameters:UnknownKey', ...
+                    'Parameter "%s" does not exist.', key);
+            end
+
+            value = obj.Params.(key).Value;
+        end
         
+        %% Assigns A New Value To A Parameter Using Its Key
+        function setValue(obj,key,val)
+
+            key = char(key);
+
+            if ~isfield(obj.Params,key)
+                error('Parameters:UnknownKey', ...
+                    'Parameter "%s" does not exist.', key);
+            end
+
+            obj.Params.(key).Value = val;
+        end
+        %% Retrieves The Metadata Associated With A Parameter Key
+        function metaObj = getMeta(obj,key)
+
+            key = char(key);
+
+            if ~isfield(obj.Params,key)
+                error('Parameters:UnknownKey', ...
+                    'Parameter "%s" does not exist.', key);
+            end
+
+            metaObj = obj.Params.(key);
+        end
+   
+        %% User Input Request
         function PromptUser(obj)
             names = fieldnames(obj.Params);
             for k = 1:numel(names)
                 param = obj.Params.(names{k});
                 while true
                     try
-                        if isprop(param, 'Unit') && ~ismissing(param.Unit) && param.Unit ~= ""
-                            promptStr = sprintf("Enter %s (%s): ", param.Name, param.Unit);
+                        if isprop(param, 'Unit') && ...
+                            ~ismissing(param.Unit) && param.Unit ~= ""
+                            promptStr = sprintf("Enter %s (%s): ", ...
+                                param.Name, param.Unit);
                         else
                             promptStr = sprintf("Enter %s: ", param.Name);
                         end
@@ -54,95 +96,135 @@ classdef Parameters < handle
         end
     end
 
-    %% ===================================================
-    %% CUSTOM INDEXING OVERLOADS FOR DIRECT SYNTAX ACCESS
-    %% ===================================================
+    %% =============================================
+    %% CUSTOM DISPLAY PATTERNS IN THE COMMAND WINDOW
+    %% =============================================
     methods (Access = protected)
-        %% Intercept Property Reference (Reading: value = obj.Fs)
-        function varargout = subsref(obj, s)
-            switch s(1).type
-                case '.'
-                    % Handle standard internal method execution (like obj.PromptUser())
-                    if ismethod(obj, s(1).subs)
-                        if nargout > 0
-                            [varargout{1:nargout}] = builtin('subsref', obj, s);
-                        else
-                            builtin('subsref', obj, s);
-                        end
-                        return;
-                    end
-                    
-                    % Route custom parameter property reads directly to .Value
-                    if isfield(obj.Params, s(1).subs)
-                        % Extract Meta object reference
-                        metaObj = obj.Params.(s(1).subs);
-                        
-                        % Support trailing chain indices like obj.Fs or obj.Fs.Unit
-                        if numel(s) > 1
-                            [varargout{1:nargout}] = subsref(metaObj, s(2:end));
-                        else
-                            varargout{1} = metaObj.Value;
-                        end
-                    else
-                        % Default fallback error for non-existent properties
-                        error('Parameters:InvalidProperty', ...
-                            'Property "%s" does not exist.', s(1).subs);
-                    end
-                    
-                otherwise
-                    % Handle array syntax tracking fallbacks (e.g. obj(1))
-                    [varargout{1:nargout}] = builtin('subsref', obj, s);
+        function displayScalarObject(obj)
+            %% Print the default class header line
+            header = getHeader(obj);
+            disp(header);
+            
+            %% Extract keys and preallocate data arrays for table display
+            keys = fieldnames(obj.Params);
+            nParams = numel(keys);
+            
+            Names = strings(nParams, 1);
+            Values = zeros(nParams, 1); 
+            DataTypes = strings(nParams, 1);
+            Units = strings(nParams, 1);
+            
+            for k = 1:nParams
+                metaObj = obj.Params.(keys{k});
+                Names(k) = metaObj.Name;
+                Values(k) = double(metaObj.Value);     
+                DataTypes(k) = metaObj.DataType;
+                
+                %% Safely Parse Unassigned Or Empty Strings
+                if ismissing(metaObj.Unit) || metaObj.Unit == ""
+                    Units(k) = "-";
+                else
+                    Units(k) = metaObj.Unit;
+                end
             end
+            
+            %% Readable Overview Table
+            T = table(Names, Values, DataTypes, Units, 'RowNames', keys);
+            T.Properties.VariableNames = {'Parameter Name', 'Value', ...
+                'Data Type', 'Unit'};
+            
+           %% Display the Table Block Directly Into the Command Line Window
+            disp(T);
         end
-
-        %% Intercept Property Assignment (Writing: obj.Fs = 10)
-        function obj = subsasgn(obj, s, val)
-            switch s(1).type
-                case '.'
-                    if isfield(obj.Params, s(1).subs)
-                        metaObj = obj.Params.(s(1).subs);
-                        
-                        % Support standard parameter value writes or explicit property modifications
-                        if numel(s) > 1
-                            subsasgn(metaObj, s(2:end), val);
-                        else
-                            metaObj.Value = val;
-                        end
-                    else
-                        error('Parameters:InvalidProperty', ...
-                            'Property "%s" does not exist.', s(1).subs);
-                    end
-                otherwise
-                    obj = builtin('subsasgn', obj, s, val);
-            end
-        end
-    end
+    end   
     
-    methods
-        %% Expose dynamic names to workspace tab-completion & properties()
-        function p = properties(obj)
-            p = fieldnames(obj.Params);
-        end
-        
-        function p = propertynames(obj)
-            p = fieldnames(obj.Params);
-        end
-    end
-
+    %% Parameters Configuration
     methods (Static, Access = private)
         function Schema = GetSchema()
             Schema = {
-                struct('Key', "Fs", 'Name', "Sampling Frequency", 'Validator', @mustBePositive, 'DataType', "double", 'Unit', "Hz")
-                struct('Key', "FData", 'Name', "Data Frequency", 'Validator', @mustBePositive, 'DataType', "double", 'Unit', "Hz")
-                struct('Key', "Fnoise", 'Name', "Noise Frequency", 'Validator', @mustBePositive, 'DataType', "double", 'Unit', "Hz")
-                struct('Key', "FcLow", 'Name', "Low Cutoff Frequency", 'Validator', @mustBePositive, 'DataType', "double", 'Unit', "Hz")
-                struct('Key', "FcHigh", 'Name', "High Cutoff Frequency", 'Validator', @mustBePositive, 'DataType', "double", 'Unit', "Hz")
-                struct('Key', "DC", 'Name', "DC Amplitude", 'Validator', @mustBePositive, 'DataType', "double", 'Unit', "Volts")
-                struct('Key', "DF", 'Name', "Downsampling Factor", 'Validator', @mustBePositive, 'DataType', "double")
-                struct('Key', "TP", 'Name', "AGC Target Peak", 'Validator', @mustBePositive, 'DataType', "double")
-                struct('Key', "Gain", 'Name', "Initial AGC Gain", 'Validator', @mustBePositive, 'DataType', "double")
-                struct('Key', "ART", 'Name', "AGC Release Time", 'Validator', @mustBePositive, 'DataType', "double", 'Unit', "Seconds")
-                struct('Key', "AAT", 'Name', "AGC Attack Time", 'Validator', @mustBePositive, 'DataType', "double", 'Unit', "Seconds")
+                struct( ...
+                'Key', "Fs", ...
+                'Name', "Sampling Frequency", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double", ...
+                'Unit', "Hz" ...
+                )
+
+                struct( ...
+                'Key', "FData", ...
+                'Name', "Data Frequency", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double", ...
+                'Unit', "Hz" ...
+                )
+
+                struct( ...
+                'Key', "Fnoise", ...
+                'Name', "Noise Frequency", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double", ...
+                'Unit', "Hz" ...
+                )
+
+                struct( ...
+                'Key', "FcLow", ...
+                'Name', "Low Cutoff Frequency", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double", ...
+                'Unit', "Hz" ...
+                )
+
+                struct( ...
+                'Key', "FcHigh", ...
+                'Name', "High Cutoff Frequency", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double", ...
+                'Unit', "Hz" ...
+                )
+
+                struct( ...
+                'Key', "DC", ...
+                'Name', "DC Amplitude", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double", ...
+                'Unit', "Volts" ...
+                )
+
+                struct( ...
+                'Key', "DF", ...
+                'Name', "Downsampling Factor", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double" ...
+                )
+
+                struct( ...
+                'Key', "TP", ...
+                'Name', "AGC Target Peak", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double" ...
+                )
+
+                struct( ...
+                'Key', "Gain", ...
+                'Name', "Initial AGC Gain", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double" ...
+                )
+
+                struct( ...
+                'Key', "ART", ...
+                'Name', "AGC Release Time", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double", ...
+                'Unit', "Seconds" ...
+                )
+                struct( ...
+                'Key', "AAT", ...
+                'Name', "AGC Attack Time", ...
+                'Validator', @mustBePositive, ...
+                'DataType', "double", ...
+                'Unit', "Seconds" ...
+                )
             };
         end
     end
